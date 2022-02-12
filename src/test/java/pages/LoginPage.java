1da54f5e21400639e0db6dd00e6a0a696ecd0e02
imran <imrananwar.sayyed@gmail.com>
2022-02-12 15:19:43 +0530

    Fist commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+
+    private final WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public By SIGN_IN = By.xpath("//*[@id='nav-link-accountList']");
+    public By SIGN_IN_TITLE = By.xpath("//*[@class='a-spacing-small']");
+
+    public WebElement clickSignIn() {
+        return driver.findElement(SIGN_IN);
+    }
+
+    public WebElement verifySignInTitle() {
+        return driver.findElement(SIGN_IN_TITLE);
+    }
+
+}
---
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -0,0 +1,47 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+
+public class stepDefinition {
+
+    static String driverPath = System.getProperty("user.dir") + "/src/test/drivers/";
+    static LoginPage loginPage = getLoginPage();
+
+    public static LoginPage getLoginPage() {
+        System.out.println("Launching google chrome with new profile..");
+        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.amazon.in/");
+        return new LoginPage(driver);
+    }
+
+
+    @Given("^User is at login page$")
+    public void user_is_at_login_page() {
+        loginPage.clickSignIn().click();
+        Assertions.assertThat(loginPage.verifySignInTitle().getText().equals("Sign-In"));
+    }
+
+    @When("^User login into application with username and password$")
+    public void user_login_into_application_with_username_and_password() {
+        System.out.println("User login into application with username and password");
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() {
+        System.out.println("Home page is populated");
+    }
+
+    @And("^Cards are displayed$")
+    public void cards_are_displayed() {
+        System.out.println("Cards are displayed");
+    }
+}
